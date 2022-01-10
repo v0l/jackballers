7b98759d145b22bc8844ef5b474c80b6b1cc0c5d
Kieran <kieran@harkin.me>
2022-01-10 18:37:33 +0000

    Update disclaimer

--- spa/src/App.js
+++ spa/src/App.js
@@ -27,10 +27,15 @@ function App() {
 
             {collections.map(a => <Collection data={a} key={a.id}/>)}
             <div className="footer">
-                <small>
-                    This site is not affilated with <a href="https://strike.me" className="txt-yellow">Strike</a> or <a href="https://twitter.com/JackMallers" className="txt-yellow">Jack Mallers</a>. 
-                    This is a parody site, all funds will be donated to <a href="https://hrf.org/" className="txt-yellow">HRF</a>.
-                </small>
+                <p>
+                    <small>
+                        This site is not affilated with <a href="https://strike.me" className="txt-yellow">Strike</a> or <a href="https://twitter.com/JackMallers" className="txt-yellow">Jack Mallers</a>.
+                        This is a parody site, all funds will be donated to <a href="https://hrf.org/" className="txt-yellow">HRF</a>.
+                    </small>
+                </p>
+                <p>
+                    <small>Made by <a href="https://github.com/v0l" className={"txt-yellow"}>v0l</a>.</small>
+                </p>
             </div>
         </div>
     );
